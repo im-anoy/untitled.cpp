2dffe3779b6c4e4b3833f17ae2e3b165c045d703
im-anoy <135540410+im-anoy@users.noreply.github.com>
2024-08-22 10:58:04 +0600

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int glo=6; 
+void sum(){
+    int a; 
+    cout<<glo;
+}
+int main (){
+    int glo=9;
+    glo=78;
+    //int a=14; 
+    //int a=35;
+    int a=14,b=76;
+    float pi=3.14;
+    char c='d';
+    sum();
+    cout<<glo;
+    //cout<<"this is tutorial 4.n\ here the value of a is "<<a<<".n\the value of b is "<<b;
+    return 0;
+    
+}
